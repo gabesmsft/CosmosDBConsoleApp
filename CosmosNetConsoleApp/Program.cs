526c1f4d4ae1692e4e1f83399a8257f53f865881
Gabe Shapiro <gabeshap@outlook.com>
2019-11-27 10:56:06 -0800

    added read method

--- CosmosNetConsoleApp/Program.cs
+++ CosmosNetConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Microsoft.Azure.Cosmos;
 using System.Configuration;
+using System.Collections.Generic;
 
 namespace CosmosNetConsoleApp
 {
@@ -26,7 +27,15 @@ namespace CosmosNetConsoleApp
         {
             Program p = new Program();
             await p.InitializeAsync();
-            await p.AddItemsToContainerAsync();
+            
+            Console.Write("Press 1 to read all items from collection, or press 2 to add an item to the collection:\n");
+            string choice= Console.ReadLine();
+            if (choice == "1")
+                await p.QueryItemsAsync();
+            else if (choice == "2")
+                await p.AddItemsToContainerAsync();
+            else
+                Console.Write("Neither 1 nor 2 were pressed, application exited.");
         }
 
         public async Task InitializeAsync()
@@ -48,6 +57,28 @@ namespace CosmosNetConsoleApp
 
             };
             ItemResponse<someEntity> eResponse = await  container.CreateItemAsync<someEntity>(e, new PartitionKey(e.someProperty));
+        }
+
+        private async Task QueryItemsAsync()
+        {
+            var sqlQueryText = "SELECT * FROM c";
+
+            Console.WriteLine("Running query: {0}\n", sqlQueryText);
+
+            QueryDefinition queryDefinition = new QueryDefinition(sqlQueryText);
+            FeedIterator<someEntity> queryResultSetIterator = container.GetItemQueryIterator<someEntity>(queryDefinition);
+
+            List<someEntity> entities = new List<someEntity>();
+
+            while (queryResultSetIterator.HasMoreResults)
+            {
+                FeedResponse<someEntity> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                foreach (someEntity entity in currentResultSet)
+                {
+                    entities.Add(entity);
+                    Console.WriteLine("\tRead {0}\n", entity);
+                }
+            }
         }
     }
 
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
-Simple test cosmosdb .NET console app for testing/ troubleshooting purposes only. This app puts an item into a CosmosDB collection.
+Simple test cosmosdb .NET console app for testing/ troubleshooting purposes only. This app either puts an item into a CosmosDB collection, or reads all items from the collection, depending upno which option you choose.
 
-Nuget-install the following packages:
+Restore or Nuget-install the following packages:
 Newtonsoft.Json;
 Microsoft.Azure.Cosmos
 
@@ -10,5 +10,6 @@ Modify the someEntity class in Program.cs according to the type of entity.
 
 Build using C# 7.1 or later.
 
-Run, and when prompted enter the values (id and someProperty (which map to the document id and partition key respectively, but you can change to something else in the someEntity class)).
+Run, and when prompted either press 1 to read all items from the collection or press 2 to add an item to the collection.
+If you choose to add an item to the collection, enter the values (id and someProperty (which map to the document id and partition key respectively, but you can change to something else in the someEntity class)).
 
